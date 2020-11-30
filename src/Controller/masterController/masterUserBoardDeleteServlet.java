56ac9c75d9a5ec24d92d1dace1b77abba8f27469
sakurai_sakura <hgf107103@gmail.com>
2020-11-30 09:05:23 +0900

    2020 11 30 9:05

--- src/Controller/masterController/masterUserBoardDeleteServlet.java
+++ src/Controller/masterController/masterUserBoardDeleteServlet.java
@@ -12,24 +12,25 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 
-import Object.commentVO;
+import Object.boardVO;
 import Object.userVO;
 import Module.databaseModule.MyBatisConnectionFactory;
 
-
-@WebServlet("/master/user/comment/delete")
-public class masterUserCommentDeleteListServlet extends HttpServlet {
+/**
+ * Servlet implementation class masterUserBoardDeleteListServlet
+ */
+@WebServlet("/master/user/board/delete")
+public class masterUserBoardDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-    public masterUserCommentDeleteListServlet() {
+    public masterUserBoardDeleteServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendError(400);
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
@@ -39,50 +40,59 @@ public class masterUserCommentDeleteListServlet extends HttpServlet {
 		SqlSession sqlse = MyBatisConnectionFactory.getSqlSession();
 		try {
 			
-			System.out.println("masterUserCommentDeleteListServlet : " + request.getParameter("userNumber"));
-			int commentNumber = Integer.parseInt(request.getParameter("commentNumber"));
+			System.out.println("masterUserBoardDeleteListServlet : " + request.getParameter("userNumber"));
 			int boardNumber = Integer.parseInt(request.getParameter("boardNumber"));
 			int userNumber = Integer.parseInt(request.getParameter("userNumber"));
 			String check = "fail";
-			
-			if (session.getAttribute("userLogin") != null) { //세션에 로그인 정보가 있을 경우
+			if (session.getAttribute("userLogin") != null) {
 				userVO userTemp = (userVO)(session.getAttribute("userLogin"));
 				userVO userCheck = sqlse.selectOne("userMapper.isAdminUserCheck", userTemp);
 				
-				if (userCheck == null) {//어드민이 아닐경우
+				if (userCheck == null) {
 					pw.write("{");
 					pw.write("\"check\":\"noAdmin\"");
 					pw.write("}");
 				}
 				
-				if (userCheck != null) {//어드민일 경우
+				if (userCheck != null) {
+					boardVO boardTemp = new boardVO();
+					boardTemp.setBoardNumber(boardNumber);
+					boardTemp.setUserNumber(userNumber);
+					//이 보드 넘버에 해당하는 댓글이 있는지 확인.
+					boardVO boardCommentCheck = sqlse.selectOne("boardMapper.callOneBoard", boardTemp);
 					
-					commentVO commentTemp = new commentVO();
-					commentTemp.setBoardNumber(boardNumber);
-					commentTemp.setCommentNumber(commentNumber);
-					commentTemp.setUserNumber(userNumber);
-					int deleteCheck = sqlse.delete("commentMapper.deleteComment", commentTemp);
-					
-					if(deleteCheck > 0) {
-						check = "success";
-						sqlse.commit();
+					int nextCheck = 0;
+					if (boardCommentCheck.getCommentCount() > 0) {
+						int commentDeleteCheck = sqlse.delete("commentMapper.deleteAllComment", boardCommentCheck.getBoardNumber());
+						if (commentDeleteCheck > 0) {
+							sqlse.commit();
+						} else if(commentDeleteCheck <= 0) {
+							check = "commentDeleteFail";
+							nextCheck = 1;
+						}
 					}
-					if(deleteCheck <= 0) {
-						check = "fail";
+					
+					if(nextCheck == 0) {
+						int deleteResult = sqlse.delete("boardMapper.deleteBoard", boardTemp);
+						if (deleteResult > 0) {
+							sqlse.commit();
+							check = "success";
+						} else if (deleteResult <= 0) {
+							check = "fail";
+						}
 					}
 				}
-			} else if(session.getAttribute("userLogin") == null) { //세션에 로그인 정보가 없을 경우
+			} else if(session.getAttribute("userLogin") == null) {
 				check = "notLogin";
 			}
-			
 			sqlse.close();
 			pw.write("{");
 			pw.write("\"check\":\""+ check +"\",");
-			pw.write("\"commentNumber\":\""+ commentNumber + "\"");
+			pw.write("\"boardNumber\":\""+ boardNumber + "\"");
 			pw.write("}");
 			
 		} catch (Exception e) {
-			System.out.println("masterUserCommentDeleteListServlet ERROR : " + e);
+			System.out.println("masterUserBoardDeleteListServlet ERROR : " + e);
 			sqlse.close();
 			response.sendError(400);
 		}
--- src/Controller/masterController/masterUserCommentDeleteServlet.java
+++ src/Controller/masterController/masterUserCommentDeleteServlet.java
@@ -12,25 +12,24 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 
-import Object.boardVO;
+import Object.commentVO;
 import Object.userVO;
 import Module.databaseModule.MyBatisConnectionFactory;
 
-/**
- * Servlet implementation class masterUserBoardDeleteListServlet
- */
-@WebServlet("/master/user/board/delete")
-public class masterUserBoardDeleteListServlet extends HttpServlet {
+
+@WebServlet("/master/user/comment/delete")
+public class masterUserCommentDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public masterUserBoardDeleteListServlet() {
+    
+    public masterUserCommentDeleteServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-    
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendError(400);
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
@@ -40,59 +39,50 @@ public class masterUserBoardDeleteListServlet extends HttpServlet {
 		SqlSession sqlse = MyBatisConnectionFactory.getSqlSession();
 		try {
 			
-			System.out.println("masterUserBoardDeleteListServlet : " + request.getParameter("userNumber"));
+			System.out.println("masterUserCommentDeleteListServlet : " + request.getParameter("userNumber"));
+			int commentNumber = Integer.parseInt(request.getParameter("commentNumber"));
 			int boardNumber = Integer.parseInt(request.getParameter("boardNumber"));
 			int userNumber = Integer.parseInt(request.getParameter("userNumber"));
 			String check = "fail";
-			if (session.getAttribute("userLogin") != null) {
+			
+			if (session.getAttribute("userLogin") != null) { //세션에 로그인 정보가 있을 경우
 				userVO userTemp = (userVO)(session.getAttribute("userLogin"));
 				userVO userCheck = sqlse.selectOne("userMapper.isAdminUserCheck", userTemp);
 				
-				if (userCheck == null) {
+				if (userCheck == null) {//어드민이 아닐경우
 					pw.write("{");
 					pw.write("\"check\":\"noAdmin\"");
 					pw.write("}");
 				}
 				
-				if (userCheck != null) {
-					boardVO boardTemp = new boardVO();
-					boardTemp.setBoardNumber(boardNumber);
-					boardTemp.setUserNumber(userNumber);
-					//이 보드 넘버에 해당하는 댓글이 있는지 확인.
-					boardVO boardCommentCheck = sqlse.selectOne("boardMapper.callOneBoard", boardTemp);
+				if (userCheck != null) {//어드민일 경우
 					
-					int nextCheck = 0;
-					if (boardCommentCheck.getCommentCount() > 0) {
-						int commentDeleteCheck = sqlse.delete("commentMapper.deleteAllComment", boardCommentCheck.getBoardNumber());
-						if (commentDeleteCheck > 0) {
-							sqlse.commit();
-						} else if(commentDeleteCheck <= 0) {
-							check = "commentDeleteFail";
-							nextCheck = 1;
-						}
+					commentVO commentTemp = new commentVO();
+					commentTemp.setBoardNumber(boardNumber);
+					commentTemp.setCommentNumber(commentNumber);
+					commentTemp.setUserNumber(userNumber);
+					int deleteCheck = sqlse.delete("commentMapper.deleteComment", commentTemp);
+					
+					if(deleteCheck > 0) {
+						check = "success";
+						sqlse.commit();
 					}
-					
-					if(nextCheck == 0) {
-						int deleteResult = sqlse.delete("boardMapper.deleteBoard", boardTemp);
-						if (deleteResult > 0) {
-							sqlse.commit();
-							check = "success";
-						} else if (deleteResult <= 0) {
-							check = "fail";
-						}
+					if(deleteCheck <= 0) {
+						check = "fail";
 					}
 				}
-			} else if(session.getAttribute("userLogin") == null) {
+			} else if(session.getAttribute("userLogin") == null) { //세션에 로그인 정보가 없을 경우
 				check = "notLogin";
 			}
+			
 			sqlse.close();
 			pw.write("{");
 			pw.write("\"check\":\""+ check +"\",");
-			pw.write("\"boardNumber\":\""+ boardNumber + "\"");
+			pw.write("\"commentNumber\":\""+ commentNumber + "\"");
 			pw.write("}");
 			
 		} catch (Exception e) {
-			System.out.println("masterUserBoardDeleteListServlet ERROR : " + e);
+			System.out.println("masterUserCommentDeleteListServlet ERROR : " + e);
 			sqlse.close();
 			response.sendError(400);
 		}
